7943cddf498a4fa8e6c13c3b51a811b1039e2799
Leo Meza <leo.e.meza@gmail.com>
2020-07-23 10:35:37 -0700

    Add image views

--- HelloTriangle/main.cpp
+++ HelloTriangle/main.cpp
@@ -91,6 +91,7 @@ private:
     std::vector<VkImage>     swapChainImages;
     VkFormat                 swapChainImageFormat;
     VkExtent2D               swapChainExtent;
+    std::vector<VkImageView> swapChainImageViews;
 
     GLFWwindow* window;
 
@@ -179,6 +180,11 @@ private:
 
     void cleanup() {
 
+        // Destroy the image views we created
+        for (auto imageView : swapChainImageViews) {
+            vkDestroyImageView(device, imageView, nullptr);
+        }
+
         // Destroy the swap chain before the logical device is destroyed
         vkDestroySwapchainKHR(device, swapChain, nullptr);
 
@@ -200,6 +206,34 @@ private:
         glfwDestroyWindow(window);
 
         glfwTerminate();
+    }
+
+    void createImageViews() {
+
+        swapChainImageViews.resize(swapChainImages.size());
+
+        for (size_t i = 0; i < swapChainImages.size(); i++) {
+
+            VkImageViewCreateInfo createInfo{};
+
+            createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+            createInfo.image = swapChainImages[i];
+            createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            createInfo.format = swapChainImageFormat;
+            createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            createInfo.subresourceRange.baseMipLevel = 0;
+            createInfo.subresourceRange.levelCount = 1;
+            createInfo.subresourceRange.baseArrayLayer = 0;
+            createInfo.subresourceRange.layerCount = 1;
+
+            if (vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS) {
+                throw std::runtime_error("failed to create image views!");
+            }
+        }
     }
 
     void createInstance() {
@@ -454,6 +488,8 @@ private:
         createLogicalDevice();
 
         createSwapChain();
+
+        createImageViews();
     }
 
     void initWindow() {
